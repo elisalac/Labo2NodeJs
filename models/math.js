63945c9f3a01372e2abaad5045b4746346215176
elisalac <95500009+elisalac@users.noreply.github.com>
2023-10-09 13:45:34 -0400

    Add files via upload

---
+++ models/math.js
@@ -0,0 +1,14 @@
+import Model from './model.js';
+
+export default class Math extends Model {
+    constructor() {
+        super();
+
+        this.addField('op', 'string');
+        this.addField('x', 'integer');
+        this.addField('y', 'integer');
+        this.addField('n', 'integer');
+
+        this.setKey("op");
+    }
+}
